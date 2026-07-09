b4803e9b302180f7886b33ae5b6e95f9a4058c91
Megha <meghareddy.mahareddy@blackbaud.com>
2026-07-09 18:27:30 +0530

    Week 2: Angular frontend connected to API - Journal Entries displaying live database data

---
+++ frontend/fundflow-client/src/app/services/api.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Api {
+  private baseUrl = 'https://localhost:7129/api';
+
+  constructor(private http: HttpClient) { }
+
+  getJournalEntries(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/JournalEntry`);
+  }
+
+  getBankTransactions(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/Treasury`);
+  }
+
+  getInvoices(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/AccountsPayable`);
+  }
+
+  getGrants(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/Grants`);
+  }
+
+  getBudgets(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/Budgeting`);
+  }
+}
